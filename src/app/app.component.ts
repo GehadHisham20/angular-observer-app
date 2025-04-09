07135d56be232616705cbd874ef2527bcb1f4e26
Gehad Hisham <Gehad_Hisham@Tascairo.com>
2025-04-09 14:06:18 +0200

    of and from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +11,34 @@ export class AppComponent {
 
   data: any[] = [];
 
-  myObserver = new Observable((observer) => {
-    setTimeout(() => {
-      observer.next('Data 1');
-    }, 1000);
+  // myObserver = new Observable((observer) => {
+  //   setTimeout(() => {
+  //     observer.next('Data 1');
+  //   }, 1000);
 
-    setTimeout(() => {
-      observer.next('Data 2');
-    }, 2000);
-    setTimeout(() => {
-      observer.next('Data 3');
-    }, 3000);
-    // setTimeout(() => {
-    //   observer.complete();
-    // }, 3500);
-    // setTimeout(() => {
-    //   observer.error('Error');
-    // }, 3500);
-    setTimeout(() => {
-      observer.next('Data 4');
-    }, 4000);
-    setTimeout(() => {
-      observer.next('Data 5');
-    }, 5000);
-  });
+  //   setTimeout(() => {
+  //     observer.next('Data 2');
+  //   }, 2000);
+  //   setTimeout(() => {
+  //     observer.next('Data 3');
+  //   }, 3000);
+  //   // setTimeout(() => {
+  //   //   observer.complete();
+  //   // }, 3500);
+  //   // setTimeout(() => {
+  //   //   observer.error('Error');
+  //   // }, 3500);
+  //   setTimeout(() => {
+  //     observer.next('Data 4');
+  //   }, 4000);
+  //   setTimeout(() => {
+  //     observer.next('Data 5');
+  //   }, 5000);
+  // });
+
+  // myObserver = of(['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5'], 12, 123);
+
+  myObserver = from(['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5']);
 
   getData() {
     this.myObserver.subscribe(
